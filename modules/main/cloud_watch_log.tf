644ed6ff7e16f16f8d90796ec43132ed9ae8b79e
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-07-26 08:20:17 +0900

    - WAFを削除しコストダウン - CloudWatch Logsに期限設定しコストダウン - ECSタスクが停止した場合にCloudWatch Logsにも記録するようにする

--- modules/main/cloud_watch_log.tf
+++ modules/main/cloud_watch_log.tf
@@ -2,3 +2,13 @@ resource "aws_cloudwatch_log_group" "ecs_rails_web" {
   name              = var.app_name
   retention_in_days = 14
 }
+
+resource "aws_cloudwatch_log_group" "rds" {
+  name              = "/aws/rds/instance/${aws_db_instance.rds.identifier}/postgresql"
+  retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_group" "ecs_task_stopped" {
+  name              = "/aws/events/ecs/task_stopped"
+  retention_in_days = 14
+}
--- modules/main/event_bridge.tf
+++ modules/main/event_bridge.tf
@@ -3,9 +3,16 @@ resource "aws_cloudwatch_event_rule" "ecs_task_stopped" {
   event_pattern = file("${path.module}/files/json/event_pattern/ecs_task_stopped.json")
 }
 
-resource "aws_cloudwatch_event_target" "ecs_task_stopped" {
-  target_id  = "ecs_task_stopped"
+resource "aws_cloudwatch_event_target" "ecs_task_stopped_to_sns" {
+  target_id  = "ecs_task_stopped_to_sns"
   rule       = "ecs-task-stopped"
   arn        = aws_sns_topic.error.arn
+  depends_on = [aws_cloudwatch_event_rule.ecs_task_stopped]
+}
+
+resource "aws_cloudwatch_event_target" "ecs_task_stopped_to_cloud_watch_logs" {
+  target_id  = "ecs_task_stopped_to_cloud_watch_logs"
+  rule       = "ecs-task-stopped"
+  arn        = aws_cloudwatch_log_group.ecs_task_stopped.arn
   depends_on = [aws_cloudwatch_event_rule.ecs_task_stopped]
 }
