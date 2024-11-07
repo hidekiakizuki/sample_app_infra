a1faf1d3d222943be7481ac5a2556c847b512e11
Hideki Akizuki <hideki.akizuki.go@gmail.com>
2024-11-07 22:15:59 +0900

    * ECRのライフサイクルポリシー設定 * ECSのworkerにサーキットブレーカー設定

--- modules/main/ecr.tf
+++ modules/main/ecr.tf
@@ -21,3 +21,31 @@ resource "aws_ecr_repository" "web_server" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+  policy     = data.aws_ecr_lifecycle_policy_document.default.json
+}
+
+resource "aws_ecr_lifecycle_policy" "web_server" {
+  repository = aws_ecr_repository.web_server.name
+  policy     = data.aws_ecr_lifecycle_policy_document.default.json
+}
+
+data "aws_ecr_lifecycle_policy_document" "default" {
+  rule {
+    priority    = 1
+    description = "最新の10個のタグ付きイメージを保持"
+
+    selection {
+      tag_status       = "tagged"
+      tag_pattern_list = ["*"]
+      count_type       = "imageCountMoreThan"
+      count_number     = 10
+    }
+
+    action {
+      type = "expire"
+    }
+  }
+}
